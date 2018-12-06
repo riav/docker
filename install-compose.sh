8b4560e572594839274489f1d058c74c5e30ba95
Rafael Igor Alexandre Vasco <rafael.igor@gmail.com>
2018-12-06 15:02:50 -0200

    Update install-compose.sh

--- install-compose.sh
+++ install-compose.sh
@@ -4,7 +4,8 @@ COMPOSE_VERSION="1.21.2"
 if [ ! -e /usr/local/bin/docker-compose ] || [ "$1" = "--update" ]; then
  [ "$2" != "" ] && COMPOSE_VERSION=$2
  [ -e /usr/local/bin/docker-compose ] && mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose.old
- curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+ echo "Downloading docker-compose version: $COMPOSE_VERSION"
+ curl -L'#' https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
  chmod +x /usr/local/bin/docker-compose
  docker-compose --version
 else
