ef77e5094bc654da890eab3ded4cd14c35cdd4ce
Rafael Igor Alexandre Vasco <rafael.igor@gmail.com>
2018-12-19 16:43:20 -0200

    Create gdrive.sh

---
+++ gdrive.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ZABBIX_HOME=/var/lib/zabbix
+
+FILE_ID="xpto"
+FILE_NAME="Equipamentos_monitorados"
+
+JSON_BEGIN='{"data":['
+JSON_END=']}'
+
+/bin/gdrive -c $ZABBIX_HOME/gdrive export --force --mime text/csv $FILE_ID >&-
+#cat $ZABBIX_HOME/Equipamentos_monitorados|grep ^[1-9]|sed 's/,$/,Default/g'
+#cat $ZABBIX_HOME/Equipamentos_monitorados|grep ^[1-9]|awk -F',' '{print "{\"{#DEVICE.IP}\":\""$1"\",\"{#DEVICE.VLAN}\":\""$2"\",\"{#DEVICE.NAME}\":\""$3"\",\"{#DEVICE.TYPE}\":\""$3"\"},"}'
+JSON_LIST=$(cat $ZABBIX_HOME/Equipamentos_monitorados|grep ^[1-9]|awk -F',' '{print "{\"{#DEVICE.IP}\":\""$1"\",\"{#DEVICE.VLAN}\":\""$2"\",\"{#DEVICE.NAME}\":\""$3"\"},"}')
+JSON_LIST=${JSON_LIST%','}
+
+#echo "${JSON_BEGIN}${JSON_LIST}${JSON_END}" >> /tmp/gdrive.log
+
+echo "${JSON_BEGIN}${JSON_LIST}${JSON_END}"
