5add0bb2206c85ed25276419360b5a337205d770
Rafael Igor Alexandre Vasco <rafael.igor@gmail.com>
2018-12-19 16:24:54 -0200

    Update install-composectl.sh

--- composectl/install-composectl.sh
+++ composectl/install-composectl.sh
@@ -2,3 +2,4 @@
 curl -sSL https://raw.githubusercontent.com/riav/docker/master/composectl/install-compose.sh | sh
 curl -sSL https://raw.githubusercontent.com/riav/docker/master/composectl/composectl.sh -o /usr/local/bin/composectl
 chmod +x /usr/local/bin/composectl
+[ ! -d /docker/compose ] && mkdir /docker/compose -p || echo "/docker/compose já existe!!!"
